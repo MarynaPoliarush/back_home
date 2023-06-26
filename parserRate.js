f02a6050de1bb3be58b362e0766b13aa28c3f9ec
MarynaPoliarush <106143753+MarynaPoliarush@users.noreply.github.com>
2023-06-26 14:00:11 +0300

    Update parserRate.js

--- parserRate.js
+++ parserRate.js
@@ -6,43 +6,44 @@ var kill  = require('tree-kill');
 async function getRate (){
     let currencies
 
-try{
-    const browser = await puppeteer.launch({"headless": 'new' ,"args": ["--fast-start", "--disable-extensions", "--no-sandbox"], 'ignoreDefaultArgs': [ '--enable-automation'] })
-    const newPage = await browser.newPage();
-    await newPage.goto('https://kurs.com.ua/')
+  try{
+      const browser = await puppeteer.launch({"headless": 'new' ,"args": ["--fast-start", "--disable-extensions", "--no-sandbox"], 'ignoreDefaultArgs': [ '--enable-automation'] })
+      const newPage = await browser.newPage();
+      await newPage.goto('https://finance.ua/ua/currency')
 
-   
-        let allNums = await newPage.evaluate( ()=>{
-            const nums = Array.from(document.querySelectorAll('.course '), e => e.innerText)
+    
+          let allNums = await newPage.evaluate( ()=>{
+              const nums = Array.from(document.querySelectorAll('.c2 '), e => e.innerText)
+              return nums
+          })
+          
+          
+          currencies = allNums.slice(2,4)
+
+          let allNums2 = await newPage.evaluate( ()=>{
+            const nums = Array.from(document.querySelectorAll('.c3 '), e => e.innerText)
             return nums
         })
         
-        // console.log(allNums.slice(0,8))
         
-        currencies = allNums.slice(0,8)
-    
+        currencies.push(...allNums2.slice(2,4))
+      
 
-    console.log(currencies)
+      
 
-    await browser.close()
-}catch(e){
-    console.log(e)
-}
+      await browser.close()
+  }catch(e){
+      console.log(e)
+  }
 
-process.on('exit', (code) => {
-    kill(child.pid, 'SIGTERM', function(err){
-      kill(child.pid, 'SIGKILL', function(err){
-        process.kill(-child.pid);
-      });
-    });
-  });
-
+ 
+  console.log(currencies)
     return currencies
     
 }
 
 
-
+getRate()
 
 
 module.exports = {getRate}
